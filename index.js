5bc0028f875169881d0b1e405f8c8ecb7fe8da40
Hieu <xuanhieu0012@gmail.com>
2021-08-09 11:08:57 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Hieu Nguyen';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome, ${currentUser.slice(0,1)}!`
